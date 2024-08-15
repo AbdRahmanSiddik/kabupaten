4ec67a7105193e7eaf1f312103a07b930fc8c875
anandamw <ananda1850mw@gmail.com>
2024-08-15 18:31:24 +0700

    update

--- app/Http/Controllers/UkuransController.php
+++ app/Http/Controllers/UkuransController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use PhpParser\Node\Expr\FuncCall;
 
 class UkuransController extends Controller
 {
-    //
+    public function index()
+    {
+
+
+        return view('admin.ukuran.ukuran');
+    }
 }
