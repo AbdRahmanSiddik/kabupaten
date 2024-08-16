9ede4e6c5489331fff1c8c7742892f7af7cd0ac4
anandamw <ananda1850mw@gmail.com>
2024-08-16 17:24:50 +0700

    update

--- app/Models/Settings.php
+++ app/Models/Settings.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Settings extends Model
 {
     use HasFactory;
+
+
+    protected $table = "settings ";
+    protected $guarded = ['id'];
+
+
+    
+
 }
